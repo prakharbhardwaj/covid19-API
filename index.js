04401cace20bf839f2ecbedbb722ee6d9ef3b2e8
Prakhar Prakash Bhardwaj <prkhrbhardwaj6@gmail.com>
2020-03-19 23:43:32 +0530

    Removing duplicate data

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
 const request = require('request');
 const cheerio = require("cheerio");
 
-app.get('/coronaviruses', (req, res) =>
+app.get('/covid19', (req, res) =>
 
     request("https://www.worldometers.info/coronavirus/", function (error, response, body) {
         if (error) {
@@ -16,18 +15,18 @@ app.get('/coronaviruses', (req, res) =>
             country[index] = {};
             country[index]['country'] = $(element).find('td:nth-child(1)').text().trim();
             country[index]['totalCases'] = $(element).find('td:nth-child(2)').text().trim();
-            country[index]['totalDeaths'] = $(element).find('td:nth-child(3)').text().trim();
-            country[index]['newDeath'] = $(element).find('td:nth-child(4)').text().trim();
-            country[index]['totalRecovered'] = $(element).find('td:nth-child(5)').text().trim();
-            country[index]['activeCases'] = $(element).find('td:nth-child(6)').text().trim();
-            country[index]['seriousCases'] = $(element).find('td:nth-child(7)').text().trim();
-            country[index]['totalCasesperOneMillionPopulation'] = $(element).find('td:nth-child(8)').text().trim();
+            country[index]['newCases'] = $(element).find('td:nth-child(3)').text().trim();
+            country[index]['totalDeaths'] = $(element).find('td:nth-child(4)').text().trim();
+            country[index]['newDeaths'] = $(element).find('td:nth-child(5)').text().trim();
+            country[index]['totalRecovered'] = $(element).find('td:nth-child(6)').text().trim();
+            country[index]['activeCases'] = $(element).find('td:nth-child(7)').text().trim();
+            country[index]['seriousCritical'] = $(element).find('td:nth-child(8)').text().trim();
         });
-
+        let index = country.findIndex(p => p.country == "Total:")
+        index += 1
+        country = country.slice(0, index)
         res.json(country);
     })
-
-
 )
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(1212, () => console.log(`App running on http://localhost:1212`))
