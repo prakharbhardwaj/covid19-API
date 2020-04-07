586d8c1632bdc599a32aa5da625adad1c5295d92
Prakhar Prakash Bhardwaj <prkhrbhardwaj6@gmail.com>
2020-04-07 19:05:56 +0530

    Revert change api status ok

--- datascrap.js
+++ datascrap.js
@@ -3,7 +3,6 @@ const cheerio = require("cheerio");
 const fetch = require('node-fetch')
 
 module.exports.scrapdata = function (req, res) {
-    /*
     fetch('https://api.covid19india.org/data.json')
         .then(res => res.json())
         .then(json => {
@@ -60,43 +59,6 @@ module.exports.scrapdata = function (req, res) {
                 res.json(country);
             })
         })
-    */
-    request("https://www.worldometers.info/coronavirus/", function (error, response, body) {
-        if (error) {
-            res.status(404).send({
-                status: false,
-                message: 'Something went wrong'
-            });
-        }
-        var country = [];
-        var $ = cheerio.load(body);
-        $('tbody>tr').each(function (index, element) {
-            country[index] = {};
-            country[index]['country'] = $(element).find('td:nth-child(1)').text().trim();
-            country[index]['totalCases'] = $(element).find('td:nth-child(2)').text().trim();
-            country[index]['newCases'] = $(element).find('td:nth-child(3)').text().trim();
-            country[index]['totalDeaths'] = $(element).find('td:nth-child(4)').text().trim();
-            country[index]['newDeaths'] = $(element).find('td:nth-child(5)').text().trim();
-            country[index]['totalRecovered'] = $(element).find('td:nth-child(6)').text().trim();
-            country[index]['activeCases'] = $(element).find('td:nth-child(7)').text().trim();
-            country[index]['seriousCritical'] = $(element).find('td:nth-child(8)').text().trim();
-        });
-        let index = country.findIndex(p => p.country == "Total:")
-        // index += 1
-        country = country.slice(0, index)
-
-        country.sort((a, b) => {
-            var keyA = a.totalCases.replace(/\,/g, '')
-            var keyB = b.totalCases.replace(/\,/g, '')
-            keyA = parseInt(keyA, 10)
-            keyB = parseInt(keyB, 10)
-            if (keyA > keyB) return -1;
-            if (keyA < keyB) return 1;
-            return 0;
-        })
-        country.push(country.shift())
-        res.json(country);
-    })
 }
 
 function formatNumber(num) {
