1d95703019fad4c8a92b9b111b0629db0a0c115e
Prakhar Prakash Bhardwaj <prkhrbhardwaj6@gmail.com>
2020-04-13 13:45:25 +0530

    Setup indian states data api

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ app.get('/covid19', datascrap.scrapdata)
 app.get('/covid19/sort', sorting.sortdata)
 app.get('/covid19/search', searching.searchdata)
 app.get('/covid19/news', news.news)
-// app.get('/covid19/statewise', india.statewise)
+app.get('/covid19/statewise', india.statewise)
 
 app.listen(port, () => console.log(`App running on http://localhost:${port}`))
--- statewise.js
+++ statewise.js
@@ -1,40 +1,31 @@
-const request = require('request');
-const cheerio = require("cheerio");
+const fetch = require('node-fetch');
+const datascrap = require('./datascrap')
+
+const formatNumber = datascrap.formatNumber
 
 module.exports.statewise = function statewise(req, res) {
+    try {
+        fetch('https://api.covid19india.org/data.json')
+            .then(res => res.json())
+            .then(json => {
+                var index = 0
+                var state = []
+                let $ = json.statewise
+                for (index in $) {
+                    state[index] = {};
+                    state[index]['state'] = formatNumber($[index].state)
+                    state[index]['totalCases'] = formatNumber($[index].confirmed)
+                    state[index]['newDeaths'] = formatNumber(`+${$[index].deltadeaths}`)
+                    state[index]['newCases'] = formatNumber(`+${$[index].deltaconfirmed}`)
+                    state[index]['totalRecovered'] = formatNumber($[index].recovered)
+                    state[index]['activeCases'] = formatNumber($[index].active)
+                    state[index]['seriousCritical'] = ''
+                }
+                res.status(200).send(state)
 
-    request("https://www.covid19india.org/", function (error, response, body) {
-        if (error) {
-            res.status(404).send({
-                status: false,
-                message: 'Something went wrong'
-            });
-        }
-        var country = [];
-        var $ = cheerio.load(body);
-        console.log($())
-        $('tbody>tr').each(function (index, element) {
-            country[index] = {};
-            country[index]['state'] = $(element).find('td:nth-child(1)').text().trim();
-            country[index]['confirmed'] = $(element).find('td:nth-child(2)').text().trim();
-            country[index]['active'] = $(element).find('td:nth-child(3)').text().trim();
-            country[index]['recovered'] = $(element).find('td:nth-child(4)').text().trim();
-            country[index]['deceased'] = $(element).find('td:nth-child(5)').text().trim();
-        });
-        // let index = country.findIndex(p => p.country == "Total:")
-        // index += 1
-        // country = country.slice(0, index)
-
-        // country.sort((a, b) => {
-        //     var keyA = a.totalCases.replace(/\,/g, '')
-        //     var keyB = b.totalCases.replace(/\,/g, '')
-        //     keyA = parseInt(keyA, 10)
-        //     keyB = parseInt(keyB, 10)
-        //     if (keyA > keyB) return -1;
-        //     if (keyA < keyB) return 1;
-        //     return 0;
-        // })
-        // country.push(country.shift())
-        res.json(country)
-    })
+            })
+    }
+    catch (err) {
+        res.status(404).send(err)
+    }
 }
