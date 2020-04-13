90015ca36356adb75566ca41306e88ea0777ded7
Prakhar Prakash Bhardwaj <prkhrbhardwaj6@gmail.com>
2020-04-13 23:55:11 +0530

    Changed logic for no new case or death

--- statewise.js
+++ statewise.js
@@ -13,18 +13,48 @@ module.exports.statewise = function statewise(req, res) {
                 let $ = json.statewise
                 for (index in $) {
                     state[index] = {};
-                    state[index]['state'] = formatNumber($[index].state)
-                    state[index]['totalCases'] = formatNumber($[index].confirmed)
-                    state[index]['totalDeaths'] = formatNumber($[index].deaths)
-                    state[index]['newDeaths'] = formatNumber(`+${$[index].deltadeaths}`)
-                    state[index]['newCases'] = formatNumber(`+${$[index].deltaconfirmed}`)
-                    state[index]['totalRecovered'] = formatNumber($[index].recovered)
-                    state[index]['activeCases'] = formatNumber($[index].active)
-                    state[index]['seriousCritical'] = ''
+                    if ($[index].deltaconfirmed == 0 && $[index].deltadeaths == 0) {
+                        state[index]['state'] = formatNumber($[index].state)
+                        state[index]['totalCases'] = formatNumber($[index].confirmed)
+                        state[index]['totalDeaths'] = formatNumber($[index].deaths)
+                        state[index]['newDeaths'] = '-'
+                        state[index]['newCases'] = '-'
+                        state[index]['totalRecovered'] = formatNumber($[index].recovered)
+                        state[index]['activeCases'] = formatNumber($[index].active)
+                        state[index]['seriousCritical'] = ''
+                    }
+                    else if ($[index].deltadeaths == 0) {
+                        state[index]['state'] = formatNumber($[index].state)
+                        state[index]['totalCases'] = formatNumber($[index].confirmed)
+                        state[index]['totalDeaths'] = formatNumber($[index].deaths)
+                        state[index]['newDeaths'] = '-'
+                        state[index]['newCases'] = formatNumber(`+${$[index].deltaconfirmed}`)
+                        state[index]['totalRecovered'] = formatNumber($[index].recovered)
+                        state[index]['activeCases'] = formatNumber($[index].active)
+                        state[index]['seriousCritical'] = ''
+                    } else if ($[index].deltaconfirmed == 0) {
+                        state[index]['state'] = formatNumber($[index].state)
+                        state[index]['totalCases'] = formatNumber($[index].confirmed)
+                        state[index]['totalDeaths'] = formatNumber($[index].deaths)
+                        state[index]['newDeaths'] = formatNumber(`+${$[index].deltadeaths}`)
+                        state[index]['newCases'] = '-'
+                        state[index]['totalRecovered'] = formatNumber($[index].recovered)
+                        state[index]['activeCases'] = formatNumber($[index].active)
+                        state[index]['seriousCritical'] = ''
+                    }
+                    else {
+                        state[index]['state'] = formatNumber($[index].state)
+                        state[index]['totalCases'] = formatNumber($[index].confirmed)
+                        state[index]['totalDeaths'] = formatNumber($[index].deaths)
+                        state[index]['newDeaths'] = formatNumber(`+${$[index].deltadeaths}`)
+                        state[index]['newCases'] = formatNumber(`+${$[index].deltaconfirmed}`)
+                        state[index]['totalRecovered'] = formatNumber($[index].recovered)
+                        state[index]['activeCases'] = formatNumber($[index].active)
+                        state[index]['seriousCritical'] = ''
+                    }
                 }
                 state.push(state.shift())
                 res.status(200).send(state)
-
             })
     }
     catch (err) {
