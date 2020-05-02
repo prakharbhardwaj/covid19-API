aa56b898ebfc18be3322a8c8455f21307d0304fc
Prakhar Prakash Bhardwaj <prkhrbhardwaj6@gmail.com>
2020-05-02 06:03:35 +0530

    Changed cron time to 8

--- cron.js
+++ cron.js
@@ -1,6 +1,6 @@
 var CronJob = require('cron').CronJob;
 const recover = require('./past_recovery')
 
-var job = new CronJob('0 6 * * *', recover.scrapdata);
+var job = new CronJob('0 8 * * *', recover.scrapdata);
 
 module.exports = job.start();
