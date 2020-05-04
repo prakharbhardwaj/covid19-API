58711d9e1f7cfe1a1343c86fb23f158b172f77a8
Prakhar Prakash Bhardwaj <prkhrbhardwaj6@gmail.com>
2020-05-04 13:37:26 +0530

    Updated recovery data: 03-05-2020 + v1.4 update link added

---
+++ src/update/mobile_update.js
@@ -0,0 +1,17 @@
+module.exports.checkUpdate = function (req, res) {
+    let ver = req.query.version
+    console.log(ver)
+    const latest_ver = 1.4
+    if (ver >= latest_ver) {
+        res.status(200).send({
+            status: true,
+            message: "Using latest version"
+        })
+    } else {
+        res.status(200).send({
+            status: false,
+            message: `A new version of CORONA Tracker is available. Please update to version ${latest_ver} now`,
+            url: 'https://i.diawi.com/xzH8si'
+        })
+    }
+}
